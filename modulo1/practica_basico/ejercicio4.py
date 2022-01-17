3930f655743b07276c060d738cbb820c1c97fa83
josther <89234051+josther@users.noreply.github.com>
2022-01-17 15:49:53 +0000

    commit & push

--- modulo1/practica_basico/ejercicio4.py
+++ modulo1/practica_basico/ejercicio4.py
@@ -1,1 +1,13 @@
 #Hacer un programa que diga si un número digitado es par o impar.
+print("diga si es par o impar")
+print("digite el numero que quiera")
+numero =int(input())
+
+if(numero % 2):
+  print("el numero es impar")
+
+print("diga que numero es par")
+numero = int(input())
+
+if(numero % 5):
+  print("el numero es par")
---
+++ modulo1/practica_basico/ejericico5.py
@@ -0,0 +1,9 @@
+#Obtener el área de un cuadrado.
+print("obtener el area de un cuadrado")
+print("poner el lado del cuadrado")
+lado = int(input())
+print("poner el lado2 del cuadrado")
+lado2 = int(input())
+
+print("el area del cuadrado es")
+print(lado*lado2)
