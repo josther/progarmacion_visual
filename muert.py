7d7b68aac3a3c165ed36ab29d2c366ffc195654b
josther <89234051+josther@users.noreply.github.com>
2022-01-18 12:38:52 +0000

    commit & push

---
+++ muert.py
@@ -0,0 +1,9 @@
+#crear un programa que diga a que edad moriras
+print("dime tu edad")
+edad = int(input())
+
+if edad >50:
+  print("Donde quieres morir")
+  lugar = input()
+else:
+  print("aun eres joven")  
