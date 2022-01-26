ed207e72bb0affebe2e2f11f86c186e7ae5ecefa
josther <89234051+josther@users.noreply.github.com>
2022-01-26 14:55:20 +0000

    commit & push

--- modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
+++ modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
@@ -1,4 +1,5 @@
 # el print sirve para impirmir un mensaje en la pantalla
+
 print("Nombre del autor")
 print("Josué David Cruz Parra")
 
@@ -6,27 +7,35 @@ print("Digite su nombre")
 nombre = input()
 print("Digite su edad")
 edad = int(input())
-
-numero = range(2)
-
 print("Digite un numeros")
 numero1 = int(input())
 
 print("Digite un numeros")
 numero2 = int(input())
 
-listado = [Suma, Resta, División, Multiplicación]
+listado = []
+suma = []
+resta = []
+multiplicación = []
+división = []
 
-listado = input()
-print("Resultado de la suma")
-print(numero1+numero2)
+for x in range(1):
+  print("colocar la suma, resta, división, multiplicación ")
 
+  if listado == suma :
+    listado = input()
+    print("Resultado de la suma")
+    print(numero1+numero2)
 
-# print("Resultado de la Resta")
-# print(numero1-numero2)
+  if listado == resta :
+    lsitado = input()
+    print("Resultado de la resta")
+    print(numero1-numero2)
 
-# print("Resultado de la Multiplicación")
-# print(numero1*numero2)
+  if listado == multiplicación:  
+    print("Resultado de la multiplicación")
+    print(numero1*numero2)
 
-# print("Resultado de la División")
-# print(numero1%numero2)
+  if listado == división:
+    print("Resultado de la división")
+    print(numero1%numero2)
