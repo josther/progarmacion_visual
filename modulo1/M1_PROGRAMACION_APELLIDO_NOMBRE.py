d22978474dd980d327ce9012484d97bc6649bb51
josther <89234051+josther@users.noreply.github.com>
2022-01-26 14:11:42 +0000

    commit $ push

--- modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
+++ modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
@@ -1,2 +1,32 @@
+# el print sirve para impirmir un mensaje en la pantalla
 print("Nombre del autor")
 print("Josué David Cruz Parra")
+
+print("Digite su nombre")
+nombre = input()
+print("Digite su edad")
+edad = int(input())
+
+numero = range(2)
+
+print("Digite un numeros")
+numero1 = int(input())
+
+print("Digite un numeros")
+numero2 = int(input())
+
+listado = [Suma, Resta, División, Multiplicación]
+
+listado = input()
+print("Resultado de la suma")
+print(numero1+numero2)
+
+
+# print("Resultado de la Resta")
+# print(numero1-numero2)
+
+# print("Resultado de la Multiplicación")
+# print(numero1*numero2)
+
+# print("Resultado de la División")
+# print(numero1%numero2)
--- modulo1/ciclo_for/ejercicio7.py
+++ modulo1/ciclo_for/ejercicio7.py
@@ -1,1 +1,7 @@
 #Generar la tabla de multiplicar pidiendo al usuario que digite el valor.
+print("digitar tablas de multiplicar")
+numero = int(input())
+
+for i in range(1, 21):
+  resultado = i * numero
+  print(numero, "x"  , i, " = ", resultado)
