a35518d16ec4e4f66129c4f2bf20f8370a145718
josther <89234051+josther@users.noreply.github.com>
2022-01-27 14:20:40 +0000

    commit & push

--- modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
+++ modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
@@ -1,5 +1,3 @@
-# el print sirve para impirmir un mensaje en la pantalla
-
 print("Nombre del autor")
 print("Josué David Cruz Parra")
 
@@ -7,27 +5,30 @@ print("Digite su nombre")
 nombre = input()
 print("Digite su edad")
 edad = int(input())
-print("Digite un numeros")
+
+print("Digite un numero")
 numero1 = int(input())
 
-print("Digite un numeros")
+print("Digite un numero")
 numero2 = int(input())
 
-operaciones = ["suma," "resta," "division," "multiplicacion"]
+operaciones = ["suma", "resta", "division", "multiplicacion",]
 
 for i in range(1):
-  print(operaciones[0])
-  print("Resultado de la suma")
-  print(numero1+numero2)
+  
+  # if (operaciones < 0):
+    operaciones = int(input())
+    print("Resultado de la suma")
+    print(numero1+numero2)
 
-  print(operaciones[1])
-  print("Resultado de la resta")
-  print(numero1-numero2)
+  # print(operaciones[1])
+  # print("Resultado de la resta")
+  # print(numero1-numero2)
  
-  print(operaciones[2])
-  print("Resultado de la multiplicacion")
-  print(numero1*numero2)
+  # print(operaciones[2])
+  # print("Resultado de la multiplicacion")
+  # print(numero1*numero2)
 
-  print(operaciones[3])
-  print("Resultado de la division")
-  print(numero1%numero2)
+  # print(operaciones[3])
+  # print("Resultado de la division")
+  # print(numero1%numero2)
