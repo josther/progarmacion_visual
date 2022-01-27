67ff177a7d6b8dc567c052f494dc98be8dc48fa2
josther <89234051+josther@users.noreply.github.com>
2022-01-27 12:32:03 +0000

    commit & push

--- modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
+++ modulo1/M1_PROGRAMACION_APELLIDO_NOMBRE.py
@@ -13,31 +13,27 @@ numero1 = int(input())
 print("Digite un numeros")
 numero2 = int(input())
 
-listado = []
-suma = []
-resta = []
-multiplicacion = []
-division = []
+operaciones = ["suma," "resta," "division," "multiplicacion"]
 
 for x in range(1):
   print("colocar la suma, resta, division, multiplicacion ")
 
-  if listado > suma :
-    listado = input()
+  if:
+    suma = input()
     print("Resultado de la suma")
     print(numero1+numero2)
 
-  if listado < resta :
-    listado = input()
-    print("Resultado de la resta")
-    print(numero1-numero2)
+  # if listado < resta :
+  #   resta = input()
+  #   print("Resultado de la resta")
+  #   print(numero1-numero2)
 
-  if listado >= multiplicacion:
-    listado = input()  
-    print("Resultado de la multiplicacion")
-    print(numero1*numero2)
+  # if listado >= multiplicacion:
+  #   multiplicacion = input()  
+  #   print("Resultado de la multiplicacion")
+  #   print(numero1*numero2)
 
-  if listado == division:
-    listado = input()
-    print("Resultado de la division")
-    print(numero1%numero2)
+  # if listado == division:
+  #   division = input()
+  #   print("Resultado de la division")
+  #   print(numero1%numero2)
