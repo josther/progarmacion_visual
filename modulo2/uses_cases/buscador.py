aa7a186686766ee2836d8b480b80926e1a3f0b26
josther <89234051+josther@users.noreply.github.com>
2022-01-28 14:36:42 +0000

    commit & push

---
+++ modulo2/uses_cases/buscador.py
@@ -0,0 +1,11 @@
+# pedir al usuario un nombre para buscar y mostrar la cantidad de resultados y los nombres y sus apellidos
+print("digita el nombre a buscar:")
+nombre = input()
+
+print("cantidadde resultados: ","3")
+
+print("{:<10} {:<10}".format("Nombres", "Apellidos"))
+print("-"*25)
+print("{:<10} {:<10}".format("Albert", "Garcia"))
+print("{:<10} {:<10}".format("Marlint", "Vasques"))
+print("{:<10} {:<10}".format("Aracelis", "Severino"))
