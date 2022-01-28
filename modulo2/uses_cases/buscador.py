1dc97a08baea8b4a8ceecddca04b0e561480f5a0
josther <89234051+josther@users.noreply.github.com>
2022-01-28 15:21:42 +0000

    modulo2

--- modulo2/uses_cases/buscador.py
+++ modulo2/uses_cases/buscador.py
@@ -2,10 +2,33 @@
 print("digita el nombre a buscar:")
 nombre = input()
 
-print("cantidadde resultados: ","3")
+
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  password="",
+  database="infotep"
+)
+
+mycursor = mydb.cursor()
+
+sql = "SELECT * FROM `usuarios` WHERE name LIKE %s"
+adr = ("%"+nombre+"%", )
+
+mycursor.execute(sql, adr)
+
+myresult = mycursor.fetchall()
+
+print("\ncantidadde resultados: ",mycursor.rowcount ,"\n")
 
 print("{:<10} {:<10}".format("Nombres", "Apellidos"))
+
 print("-"*25)
+for x in myresult:
+  print(x[0], x[1])
+  
 print("{:<10} {:<10}".format("Albert", "Garcia"))
 print("{:<10} {:<10}".format("Marlint", "Vasques"))
 print("{:<10} {:<10}".format("Aracelis", "Severino"))
