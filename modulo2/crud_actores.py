146e18cf8194e6a5a9faf05b2ecd63c8674e5ba9
josther <89234051+josther@users.noreply.github.com>
2022-02-11 15:48:37 +0000

    modulo2

--- modulo2/crud_actores.py
+++ modulo2/crud_actores.py
@@ -6,10 +6,10 @@ import os
 
 #2.establecer la conexion
 conexion = mysql.connector.connect(
-    host = "localhost",
-    user = "root",
-    passwd = "",
-    db = "sakila"
+  host = "localhost",
+  user = "root",
+  passwd = "",
+  db = "sakila"
 )
 
 #3.definir el cursor
@@ -37,24 +37,66 @@ os.system("clear")
 print("opcion eligida;", opcion)
 
 if opcion == "1":
-    print("mostrar todos los actores")
+  print("mostrar todos los actores")
 
+  sql = "SELECT first_name,last_name FROM actor;"
+
+  cursor.execute(sql)
+
+  actores = cursor.fetchall()
+
+  print("cantidad", cursor.rowcount)
+
+  for actor in actores:
+    print(actor)
 
 if opcion == "2":
-    print("Insertar nuevo actor")
+  print("Insertar nuevo actor")
+  actor = input()
+
+  print("insertar apellido del nuevo actor")
+  apellido = input()
+
+  sql = "insert into actor (first_name, last_name) values (%s,%s)"
+
+  valores = [actor, apellido]
+
+  actor = cursor.execute(sql, valores)
+
+  conexion.commit()
+
 
 
 if opcion == "3":
-    print("actualizar informacion de actor")
+  print("actualizar informacion de actor")
+  actor = input()
+
+  print("apellido del actor")
+  apellido = input()
+
+  sql = "update into actor (first_name, last_name) values (%s,%s)"
+
+  valores = [actor, apellido]
+
+  actor = cursor.execute(sql, valores)
+
+  conexion.commit()
+
+
 
 
 if opcion == "4":
     print("eliminar un actor")
+
+    sql = "delete FROM actor (first_name) values (%s)"
 
 
 if opcion == "5":
     print("buscar un actor por su id")
 
 
+
 if opcion == "6":
     print("buscar actores por nombre")            
+
+
