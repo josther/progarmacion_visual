ffbe198bf92edd8e5b71b4668147ae6f36e3a3ca
josther <89234051+josther@users.noreply.github.com>
2022-02-28 15:44:07 +0000

    modulo 4

--- modulo4/sakila/vistas/vista_actores.php
+++ modulo4/sakila/vistas/vista_actores.php
@@ -5,5 +5,5 @@ echo"<h1> $pagina </h1>";
 echo "soy el archivo vista";
 
 ?>
-
+<i class="bi bi-emoji-frown-fill"></i>
 <?php require_once "plantilla.php"; ?>
--- modulo4/sakila/vistas/vista_categorias.php
+++ modulo4/sakila/vistas/vista_categorias.php
@@ -3,3 +3,7 @@
 echo"<h1> $pagina </h1>";
 
 echo "soy el archivo vista";
+
+?>
+
+<?php require_once "plantilla.php"; ?>
